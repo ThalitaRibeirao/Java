3e226e192b3c3e2b8dcfaebb63af466d4dd76919
Thalita Silva <82406002+ThalitaRibeirao@users.noreply.github.com>
2025-01-13 11:35:02 -0300

    Update Day_1.java

--- DailyProblems/Day_1.java
+++ DailyProblems/Day_1.java
@@ -1,1 +1,13 @@
+import java.io.IOException; 
+
 // https://judge.beecrowd.com/pt/problems/view/1000
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.printf("Hello World!\n");
+ 
+    }
+ 
+}
