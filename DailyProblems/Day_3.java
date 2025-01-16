525d3c693d79a546c60a8e2ca59d646ed510ee8f
Thalita Silva <82406002+ThalitaRibeirao@users.noreply.github.com>
2025-01-16 10:32:22 -0300

    Create Day_3.java

---
+++ DailyProblems/Day_3.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+// https://judge.beecrowd.com/pt/problems/view/1002
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        double a, n, area;
+        String output;
+        
+        n = 3.14159;
+        
+        double a = input.nextDouble();
+        area = a * a * n;
+        
+        output = String.format("%.4f", area)
+        System.out.printf("A=%s\n", output);
+    }
+}
