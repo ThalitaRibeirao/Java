2f1c778a6b0ca33d56d84df51b46fb2bd8128833
Thalita Silva <82406002+ThalitaRibeirao@users.noreply.github.com>
2025-01-16 10:35:11 -0300

    Create Day_4.java

---
+++ DailyProblems/Day_4.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+// https://judge.beecrowd.com/pt/problems/view/1003
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        int a, b, soma;
+        Scanner input = new Scanner(System.in);
+        
+        a = input.nextInt();
+        b = input.nextInt();
+        soma = a + b;
+        
+        System.out.print("SOMA = %d\n", soma);
+    } 
+}
