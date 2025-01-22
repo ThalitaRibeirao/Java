442e7f1615c4cd9c9a396594b1041e2ea69169fa
Thalita Silva <82406002+ThalitaRibeirao@users.noreply.github.com>
2025-01-22 11:51:54 -0300

    Create Day_8.java

---
+++ DailyProblems/Day_8.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+// https://judge.beecrowd.com/pt/problems/view/1007
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner input;
+        int a, b, c, d, dif;
+        
+        input = new Scanner(System.in);
+        
+        int a = nextInt();
+        int b = nextInt();
+        int c = nextInt();
+        int d = nextInt();
+        
+        dif = (a * b) - (c * d);
+        
+        System.out.printf("DIFERENCA = %d\n", dif);
+ 
+    }
+ 
+}
